9154fe1fc644a526161ef965e6f6d8f02700985d
hyewon <wonny2327@gamil.com>
2024-04-17 14:11:27 +0900

    DESIGN : commonButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Button } from './components/common/Button';
 import './styles/_global.scss';
 function App() {
   return (
     <div className='app'>
-      <div>산타시작</div>
+      <div>버튼 테스트</div>
+      <div>props : variant, onClick / 택스트는 children으로 넣으세요</div>
+      <Button variant='green1'>green1 버튼입니다</Button>
+      <Button variant='gray' onClick={() => {
+        alert('버튼 클릭 동작');
+      }}>gray 버튼입니다.</Button>
+      <Button variant='green3'>green3 버튼입니다.</Button>
+      <Button variant='yellow'>yellow 버튼입니다.</Button>
+      <Button variant='rounded-outline'>rounded outline</Button>
+      <Button variant='rounded-color'>rounded color</Button>
     </div>
   );
 }
