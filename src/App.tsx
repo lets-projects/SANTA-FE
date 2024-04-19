7c7eded56909fa949eb83e35a3699755c9e8b2c4
hyewon <wonny2327@gamil.com>
2024-04-19 13:53:19 +0900

    dev merge

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './styles/_global.scss';
 import './styles/_reset.scss';
 import Navigation from './components/common/Naigation/Navigation';
+import Main from './pages/main/Main.page';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Navigation />
       <div id="wrapper">
         <Routes>
-          <Route path="/" element={<>메인페이지 입니다</>} />
+          <Route path="/" element={<Main />} />
           <Route path="/porfile" element={<>프로필 페이지 입니다</>} />
           <Route path="/rank" element={<>랭킹 페이지 입니다</>} />
           <Route path="/challenge" element={<>챌린지 페이지 입니다</>} />
