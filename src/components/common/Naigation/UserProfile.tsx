91ed7e02a026c34e8d50b105c47ceec70dec16ad
kyoung <kyoung00828@gmail.com>
2024-04-26 16:14:54 +0900

    Feat: userInfo 적용

--- src/components/common/Naigation/UserProfile.tsx
+++ src/components/common/Naigation/UserProfile.tsx
@@ -1,23 +1,28 @@
 import styles from './UserProfile.module.scss';
+import useUserInfoStore from '/src/store/userInfoStore';
 
-const user: { name: string; rank: number; img: string } = {
-  name: '산악대장',
-  rank: 1256,
-  img: 'https://cdn.pixabay.com/photo/2022/08/05/05/59/korea-7366036_1280.jpg',
-};
+// const user: { name: string; rank: number; img: string } = {
+//   name: '산악대장',
+//   rank: 1256,
+//   img: 'https://cdn.pixabay.com/photo/2022/08/05/05/59/korea-7366036_1280.jpg',
+// };
+
+const USERANK = 7777;
 
 export default function UserProfile() {
+  const { userInfo } = useUserInfoStore();
+
   return (
     <div className={styles.profileContainer}>
       <div className={styles.userProfile}>
-        <img src={user.img} />
+        <img src={userInfo.image} />
         <div className={styles.rankBox}>
           <p>내 랭킹🏅</p>
-          <p>{user.rank}점</p>
+          <p>{USERANK}점</p>
         </div>
       </div>
       <div className={styles.textBox}>
-        <p>반갑습니다 {user.name}님!</p>
+        <p>반갑습니다 {userInfo.nickname}님!</p>
         <p>오늘도 즐거운 등산 되세요😄</p>
       </div>
     </div>
--- src/store/userInfoStore.ts
+++ src/store/userInfoStore.ts
@@ -17,7 +17,13 @@ interface UserInfoActions {
   deleteUserInfo: () => void;
 }
 
-const defaultState = { email: '', nickname: '', name: '', phoneNumber: '', image: '' };
+const defaultState = {
+  email: '',
+  nickname: '비회원',
+  name: '비회원',
+  phoneNumber: '01012341234',
+  image: 'https://cdn.pixabay.com/photo/2023/10/14/09/20/mountains-8314422_1280.png',
+};
 
 const useUserInfoStore = create<UserInfoState & UserInfoActions>((set) => ({
   userInfo: defaultState,
