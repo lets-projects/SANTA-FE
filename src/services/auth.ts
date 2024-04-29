a96f34241054152319d5e042e32e2b4bdc8cc2fb
kyunghye <kyoung00828@gmail.com>
2024-04-29 17:00:09 +0900

    Feat: public, private routes 설정

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,5 @@
-import { redirect } from 'react-router-dom';
-import { paths } from '../utils/path';
+// import { redirect } from 'react-router-dom';
+// import { paths } from '../utils/path';
 
 export function getAuthToken() {
   return localStorage.getItem('access_token');
@@ -9,17 +9,17 @@ export function getRefreshToken() {
   return localStorage.getItem('refresh_token');
 }
 
-export function tokenLoader() {
-  return getAuthToken();
-}
+// export function tokenLoader() {
+//   return getAuthToken();
+// }
 
-export function checkAuthLoader() {
-  const token = getAuthToken();
+// export function checkAuthLoader() {
+//   const token = getAuthToken();
 
-  if (!token) {
-    console.log('need to login.');
-    return redirect(paths.LOGIN);
-  }
+//   if (!token) {
+//     console.log('need to login.');
+//     return redirect(paths.LOGIN);
+//   }
 
-  return null;
-}
+//   return null;
+// }
