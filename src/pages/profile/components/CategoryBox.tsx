e99a51275e771b7855eb3d35a368d693922afbc2
kyunghye <kyoung00828@gmail.com>
2024-04-30 13:04:53 +0900

    Feat: 선호 카테고리 get api 연결

--- src/pages/profile/components/CategoryBox.tsx
+++ src/pages/profile/components/CategoryBox.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-// import { useQuery } from '@tanstack/react-query';
-// import { getCategory } from '/src/services/categoryApi';
+import { useQuery } from '@tanstack/react-query';
+import { getPreferCategory } from '/src/services/categoryApi';
 
 import { FaGear } from 'react-icons/fa6';
 import styles from './CategoryBox.module.scss';
@@ -9,6 +9,17 @@ import { paths } from '/src/utils/path';
 const CATEGORY = ['출사', '힐링', '아마추어'];
 
 export default function CategoryBox() {
+  const { data: preferCategory } = useQuery({
+    queryKey: ['preferCategory'],
+    queryFn: getPreferCategory,
+    select: (data) => data.data,
+    staleTime: Infinity,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  });
+
+  console.log(preferCategory);
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>
@@ -18,8 +29,8 @@ export default function CategoryBox() {
         </Link>
       </div>
       <div className={styles.categoryList}>
-        {CATEGORY.map((category, index) => {
-          return <p key={index}>#{category}</p>;
+        {CATEGORY.map((category) => {
+          return <p key={category}>#{category}</p>;
         })}
       </div>
     </div>
