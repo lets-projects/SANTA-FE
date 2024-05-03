5a5c8f0eaf948a1507dcb834ab8baa46dff12a51
kyunghye <kyoung00828@gmail.com>
2024-05-03 17:25:18 +0900

    Feat: 나의 업적 페이지 구현 완료

--- src/pages/user/mountain/RecordMountainPage.tsx
+++ src/pages/user/mountain/RecordMountainPage.tsx
@@ -3,10 +3,14 @@ import { useQuery } from '@tanstack/react-query';
 import styles from './RecordMountainPage.module.scss';
 import { PiMedal } from 'react-icons/pi';
 import { getMyMountains } from '/src/services/challengeApi';
-import RecordCarde from './components/RecordCard';
+import RecordList from './components/RecordList';
 
 export default function RecordMountainPage() {
-  const { data: myMountains } = useQuery({
+  const {
+    data: myMountains,
+    isError,
+    isFetched,
+  } = useQuery({
     queryKey: ['myMountains'],
     queryFn: getMyMountains,
     select: (data) => data.data.content,
@@ -15,15 +19,14 @@ export default function RecordMountainPage() {
     refetchOnWindowFocus: false,
   });
 
+  const SUCCESS = isFetched && !isError;
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.title}>나의 업적</div>
         <PiMedal className={styles.icon} />
       </div>
-      <div className={styles.bottom}>
-        <RecordCarde recordData={myMountains[0]} />
-      </div>
+      <div className={styles.bottom}>{SUCCESS && <RecordList myMountains={myMountains} />}</div>
     </div>
   );
 }
