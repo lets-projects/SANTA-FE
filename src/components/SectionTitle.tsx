8e92fbfc8b4ff8de7f65714f7631c4f809a43db0
devchaeyoung <chaemaa00@gmail.com>
2024-05-05 11:30:35 +0900

    fix: 코드리뷰 반영

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -3,25 +3,24 @@ import { Link } from 'react-router-dom';
 interface SectionTitleProps {
   title: string;
   subtitle: string;
-  goToPage?: string;
+  targetPageUrl?: string;
   isThereToggle?: boolean;
 }
 
 const SectionTitle = ({
   title = '챌린지 둘러보기',
   subtitle = '업적을 달설할 수 있는 챌린지를 확인해보세요!',
-  goToPage,
+  targetPageUrl,
 }: SectionTitleProps) => {
   return (
     <div className={styles.sectionTitleWrapper}>
       <p className={styles.title}>{title}</p>
       <div className={styles.subtitleWrapper}>
         <p className={styles.subtitle}>{subtitle}</p>
-        {goToPage === undefined ? (
-          ''
-        ) : (
-          <Link to={`${goToPage}`} className={styles.goToPageButton}>
-            더보기 &gt;
+        {targetPageUrl && (
+          <Link to={`${targetPageUrl}`} className={styles.goToPageButton}>
+            {' '}
+            더보기 &gt;{' '}
           </Link>
         )}
       </div>
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -34,7 +34,7 @@ export default function Main() {
           <SectionTitle
             title="챌린지 둘러보기"
             subtitle="업적을 달설할 수 있는 챌린지를 확인해보세요!"
-            goToPage={paths.CHALLENGE}
+            targetPageUrl={paths.CHALLENGE}
           />
           <Thumbnail data={challenges} isHotTopic={true} isIndexChip={true} />
         </div>
@@ -43,12 +43,16 @@ export default function Main() {
             <div className={styles.toggleWrapper}>
               <Toggle />
             </div>
-            <SectionTitle title="신규 모임" subtitle="현재 진행중인 모임을 확인해보세요." goToPage={paths.GATHERING} />
+            <SectionTitle
+              title="신규 모임"
+              subtitle="현재 진행중인 모임을 확인해보세요."
+              targetPageUrl={paths.GATHERING}
+            />
             <MeetingList meetings={meetings} />
           </div>
         </div>
         <div>
-          <SectionTitle title="랭킹🏅" subtitle="이달의 랭킹을 확인해보세요!" goToPage={paths.RANK} />
+          <SectionTitle title="랭킹🏅" subtitle="이달의 랭킹을 확인해보세요!" targetPageUrl={paths.RANK} />
           <UserRankList ranks={ranks} />
         </div>
       </div>
