8276b63c50f7362faa30899f98c13462ffb3a143
kyunghye <kyoung00828@gmail.com>
2024-05-06 14:29:48 +0900

    Feat: 선호 카테고리 post 추후 다시 구현

--- src/pages/user/category/CategoryPage.tsx
+++ src/pages/user/category/CategoryPage.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { getAllCategory } from '/src/services/categoryApi';
+// import { useState } from 'react';
 
 import styles from './CategoryPage.module.scss';
 import { Button } from '/src/components/common/Button';
@@ -8,6 +9,7 @@ import useUserInfo from '/src/hooks/useUserInfo';
 const ICON = ['⛰️', '❓', '🌿', '🍽️', '👊🏻', '🎒', '📸', '🍁', '🌍'];
 
 export default function CategoryPage() {
+  // const [selectCategory, setSelectCategory] = useState([]);
   const { data: allCategoryData } = useQuery({
     queryKey: ['allCategoryData'],
     queryFn: getAllCategory,
@@ -23,35 +25,32 @@ export default function CategoryPage() {
     return { ...caterory, icon: ICON[index] };
   });
 
+  console.log(allCategoryData);
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <h2>{userInfo?.nickname}님, 환영해요!</h2>
-        <p>시작에 앞서 선호하시는 카테고리를 선택해주세요.</p>
+        <p>선호하시는 카테고리를 선택해주세요.</p>
       </div>
       <div className={styles.middle}>
         <div className={styles.categoryContainer}>
           <h3 className={styles.title}>목적</h3>
           <div className={styles.categoryList}>
             {newCategoryData?.map((item) => (
-              <div key={item.name} className={styles.categoryBox}>
+              <div
+                key={item.id}
+                className={styles.categoryBox}
+                onClick={(e) => {
+                  console.log(e.currentTarget);
+                }}
+              >
                 <p className={styles.icon}>{item.icon}</p>
                 <p className={styles.name}>#{item.name}</p>
               </div>
             ))}
           </div>
         </div>
-        {/* <div className={styles.categoryContainer}>
-          <h3 className={styles.title}>운동 능력</h3>
-          <div className={styles.categoryList}>
-            {PHYSICAL.map((item) => (
-              <div key={item.name} className={styles.categoryBox}>
-                <p className={styles.icon}>{item.icone}</p>
-                <p className={styles.name}>#{item.name}</p>
-              </div>
-            ))}
-          </div>
-        </div> */}
       </div>
       <div className={styles.bottom}>
         <Button variant="green1">이제 시작 해 볼까요?</Button>
