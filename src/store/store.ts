e48c087690c98e1e0ea04be063eaa6496e45938b
hyewon <wonny2327@gamil.com>
2024-07-04 15:55:10 +0900

    refactor: 불필요한 category store삭제

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,4 @@
 import { create } from 'zustand';
-import { GatheringCategoryType } from '../types/gatheringTypes';
-//카테고리 상태 관리
-interface CategoryState {
-  category: GatheringCategoryType;
-  setCategory: (category: GatheringCategoryType) => void;
-}
-
-export const useCategoryStore = create<CategoryState>((set) => ({
-  category: { id: 1, name: '등산' },
-  setCategory: (category) => set({ category: category }),
-}));
 
 interface searchValueState {
   searchValue: string;
